10ad644a9ea9c153843b785b396d91f4dc43469d
SMushonga <31137992+SMushonga@users.noreply.github.com>
2023-02-06 10:14:07 +0200

    Update config.py

--- FantasyChess/fantasy_chess/config.py
+++ FantasyChess/fantasy_chess/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    STRIPE_SECRET_KEY = 'sk_test_51MEvEEBnteUfgja4okpLxfjYGH9I6Z00vla9P7PLBF0Tf9SppzyzL8Akg3I16dRK0Lq5otLJIiKVxwz1WqkyxOUf004xNnkvsA'
+    STRIPE_SECRET_KEY = os.environ.get('Stripe_key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
