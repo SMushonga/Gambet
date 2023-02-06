23ab2c0895a8899f180bd8efd00a409ae0a55ce1
SMushonga <31137992+SMushonga@users.noreply.github.com>
2023-02-06 20:55:15 +0200

    Add files via upload

---
+++ fantasy_chess/Insert/routes.py
@@ -0,0 +1,78 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint, jsonify
+from flask_login import login_user, current_user, logout_user, login_required
+from fantasy_chess import db, admin_permission
+import stripe
+from fantasy_chess.config import Config
+from fantasy_chess.Insert.forms import Tournament_Form, Player_Form, News_Form
+from fantasy_chess.models import User, Tournament, League, Online_Team, OTB_Team, Player_Online_Team, Player_OTB_Team, Player, Historic_Online_Teams, Historic_OTB_Teams
+from datetime import datetime, time, date
+from fantasy_chess.League.functions import create_league_function
+from fantasy_chess.Payments.functions import create_product_for_league
+from flask_principal import  Permission, RoleNeed
+insert = Blueprint('insert', __name__)
+
+admin_permission = Permission(RoleNeed('admin'))
+
+stripe.api_key= Config.STRIPE_SECRET_KEY
+
+
+@insert.route('/insert_tournament', methods=['GET', 'POST'])
+@admin_permission.require()
+def insert_tournament():
+    form = Tournament_Form()
+    if form.validate_on_submit():
+        transfer_deadline = datetime.combine(form.Start.data.date(), time())
+        new_tournament = Tournament(title=form.Title.data, start = form.Start.data, end= form.End.data, tournament_type=form.Tournament_Type.data)
+        db.session.add(new_tournament)
+        create_league_function(title=form.Title.data, league_start=form.Start.data, league_end=form.End.data, user=current_user)
+
+        #A more robust query needs to be inserted here
+        league = League.query.filter_by(title=form.Title.data, league_start=form.Start.data).first()
+        league.tournament = new_tournament
+
+        try:
+            if create_product_for_league(league.id, cost=500):
+                db.session.commit()
+                return redirect(url_for('insert.insert_player_tournament', tournament_id=new_tournament.id))
+            else:
+                db.session.rollback()
+                flash(f'We could not create the tournament entry for the tournament, please re-enter this information', 'failure')
+                return redirect(url_for('insert.insert_tournament'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'{e} was the error, please try again,', 'failure')
+            return redirect(url_for('insert.insert_tournament'))
+    return render_template('insert_tournament.html', form=form)
+
+@insert.route('/insert_player', methods=['GET'])
+@login_required
+@admin_permission.require()
+def insert_player():
+    form = Player_Form()
+    if form.validate_on_submit():
+        player = Player(name=form.Name.data, rating=form.Rating.data, cost=form.Cost.data, birthdate=form.Birthdate.data, Lichess_Username=form.Lichess_Username.data, Chesscom_Username=form.Chesscom_Username.data)
+        db.session.add(player)
+        db.session.commit()
+    return render_template('insert_player.html', form=form)
+
+@insert.route('/insert_player_tournament/<tournament_id>', methods=['GET'])
+@login_required
+@admin_permission.require()
+def insert_player_tournament(tournament_id):
+    tournament = Tournament.query.get_or_404(tournament_id)
+    players = Player.query.all()
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    render_template('insert_player_tournament.html', players=players)
+
+@insert.route('/insert_news', methods=['GET'])
+@login_required
+@admin_permission.require()
+def insert_news():
+    form = News_Form()
+    if form.validate_on_submit():
+        pass
+    return render_template('insert_news.html', form=form)
+
+
+#Give out prizes 
---
+++ fantasy_chess/models.py
@@ -0,0 +1,214 @@
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from fantasy_chess import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#Association Tables define many-to-many relationships between two objects 
+#db.session.begin_nested()
+class User_League(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    league_id = db.Column(db.Integer, db.ForeignKey('league.id'), nullable=False)
+    position = db.Column(db.Integer, default=0)
+    old_position = db.Column(db.Integer, default=0)
+    points = db.Column(db.Integer, default=0)
+
+class User_Product(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+class Player_Tournament(db.Model):
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
+    tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'), nullable=False)
+    position = db.Column(db.Integer, default=0)
+    old_position = db.Column(db.Integer, default=0)
+    points = db.Column(db.Integer, default=0)
+
+class Player_Online_Team(db.Model):
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
+    online_team_id = db.Column(db.Integer, db.ForeignKey('online_team.id'), nullable=False)
+    captain = db.Column(db.Boolean,  default=False)
+
+class Player_OTB_Team(db.Model):
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
+    otb_team_id = db.Column(db.Integer, db.ForeignKey('otb_team.id'), nullable=False)
+    captain = db.Column(db.Boolean,  default=False)
+
+class Player_Historic_Online_Teams(db.Model):
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
+    online_team_id = db.Column(db.Integer, db.ForeignKey('online_team.id'), nullable=False)
+    captain = db.Column(db.Boolean,  default=False)
+    points = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, default=0)
+
+class Player_Historic_OTB_Teams(db.Model):
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
+    otb_team_id = db.Column(db.Integer, db.ForeignKey('otb_team.id'), nullable=False)
+    captain = db.Column(db.Boolean, default=False)
+    points = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, default=0)
+
+#Tables
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), nullable=False)
+    teamname = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    roles = db.Column(db.String(60), nullable=False, default='Civilian')
+    transfer_balance = db.Column(db.Integer, nullable=False, default=200)
+    transfers_available = db.Column(db.Integer, nullable=False, default=6)
+
+    account_confirmed = db.Column(db.Boolean)
+    account_confirmed_on = db.Column(db.DateTime)
+
+    membership = db.Column(db.String(120), nullable=False, default='Free')
+    leagues_created = db.Column(db.Integer, default=0)
+
+    #Lazy=True as one query is generally enough here
+    articles = db.relationship('Article', backref='author')
+    prizes = db.relationship('Prize', backref='winner')
+
+    online_team = db.relationship('Online_Team', backref='owner', uselist=False, cascade="all, delete")
+    otb_team = db.relationship('OTB_Team', backref='owner', uselist=False, cascade="all, delete")
+
+    historic_online_teams = db.relationship('Historic_Online_Teams', backref='owner', cascade="all, delete")
+    historic_otb_team = db.relationship('Historic_OTB_Team', backref='owner', cascade="all, delete")
+
+    leagues = db.relationship('User_League', backref='user')
+    products = db.relationship('User_Product', backref='user')
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    cost = db.Column(db.Integer, nullable=False)
+    birthdate = db.Column(db.DateTime)
+    status = db.Column(db.String(120), default='active')
+
+    Lichess_Username = db.Column(db.String(120))
+    Chesscom_Username = db.Column(db.String(120))
+    
+    online_teams = db.relationship('Player_Online_Team', backref='player')
+    otb_teams = db.relationship('Player_OTB_Team', backref='player')
+
+    historic_online_teams = db.relationship('Player_Historic_Online_Teams', backref='player')
+    historic_otb_teams = db.relationship('Player_Historic_OTB_Teams', backref='player')
+
+    tournaments = db.relationship('Player_Tournament', backref='player')
+
+class Online_Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
+    captain_id = db.Column(db.Integer)
+    players = db.relationship('Player_Online_Team', backref='online_team', cascade="all, delete")
+
+class Historic_Online_Teams(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
+    captain_id = db.Column(db.Integer)
+    points = db.Column(db.Integer, default=0)
+    created = db.Column(db.Datetime)
+    players = db.relationship('Player_Historic_Online_Team', backref='otb_team')
+    tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'), nullable=False)
+
+class OTB_Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
+    captain_id = db.Column(db.Integer)
+    players = db.relationship('Player_OTB_Team', backref='otb_team', cascade="all, delete")
+
+
+class Historic_OTB_Teams(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
+    captain_id = db.Column(db.Integer)
+    points = db.Column(db.Integer, default=0)
+    created = db.Column(db.Datetime)
+    players = db.relationship('Player_Historic_OTB_Team', backref='otb_team')
+    tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'), nullable=False)
+
+
+class Tournament(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    start = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+    transfer_deadline =  db.Column(db.DateTime)
+    tournament_type = db.Column(db.String(120))
+    status = db.Column(db.String(120), default='Uncommenced')
+    players = db.relationship('Player_Tournament', backref='tournament')
+    league = db.relationship('League', backref='tournament')
+    otb_teams = db.relationship('Historic_OTB_Teams', backref='tournament')
+    online_teams = db.relationship('Historic_Online_Teams', backref='tournament')
+
+
+class League(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    start = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+    league_type = db.Column(db.String(120))
+    joining_code = db.Column(db.String(120), unique=True, nullable=False)
+    status = db.Column(db.String(120), default='Uncommenced')
+    user = db.relationship('User_League', backref='league')
+    prizes = db.relationship('Prize', backref='league')
+    product = db.relationship('Product', backref='league')
+    tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.id'), nullable=False)
+
+class Prize(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.Text)
+    title = db.Column(db.String(120))
+    status = db.Column(db.String(120), default='Unawarded')
+    date_awarded = db.Column(db.DateTime)
+    value = db.Column(db.Integer)
+    winner_id = db.Column(db.Integer, db.ForeignKey('winner.id'), nullable=False)
+    league_id = db.Column(db.Integer, db.ForeignKey('league.id'), nullable=False)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    #Consider a table linking articles and tournaments/players 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Article('{self.title}', '{self.date_posted}')"
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    description = db.Column(db.Text)
+    cost = db.Column(db.Integer)
+    subscription = db.Column(db.Boolean, nullable=False, default=False)
+    stripe_price_id = db.Column(db.String(120))
+    product_id = db.Column(db.String(120))
+    period = db.Column(db.String(120), nullable=True)
+    user = db.relationship('User_Product', backref='product')
+    league_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+
